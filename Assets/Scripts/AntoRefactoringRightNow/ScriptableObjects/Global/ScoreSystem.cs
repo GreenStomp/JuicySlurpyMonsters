64313a9a2a0838035cf896a1e678f6fd37ab983c
AntonioVerdiglione <anto.verdi27@gmail.com>
2018-05-13 20:57:43 +0200

    ScoreSystem Initialized

---
+++ Assets/Scripts/AntoRefactoringRightNow/ScriptableObjects/Global/ScoreSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScoreSystem", menuName = "ScoreSystem")]
+public class ScoreSystem : ScriptableObject
+{
+    private int score;
+    public int Score { get { return score; } set { score = value; } }
+
+    private int bestScore;
+    public int BestScore { get { return bestScore; } }
+
+    public void ReleaseScore()
+    {
+        score = 0;
+    }
+    public void CheckNewBestScore()
+    {
+        int lastScore = bestScore;
+        bestScore = score > lastScore ? score : lastScore;
+    }
+}
