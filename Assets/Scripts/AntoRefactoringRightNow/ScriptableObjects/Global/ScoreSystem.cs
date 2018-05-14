675d728e6afbd1410bd6224e01d277cf2af53e3a
AntonioVerdiglione <anto.verdi27@gmail.com>
2018-05-14 14:27:27 +0200

    serialization,score system added

--- Assets/Scripts/AntoRefactoringRightNow/ScriptableObjects/Global/ScoreSystem.cs
+++ Assets/Scripts/AntoRefactoringRightNow/ScriptableObjects/Global/ScoreSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ScoreSystem", menuName = "ScoreSystem")]
@@ -7,13 +8,20 @@ public class ScoreSystem : ScriptableObject
 {
     private int score;
     public int Score { get { return score; } set { score = value; } }
-
     private int bestScore;
     public int BestScore { get { return bestScore; } }
-
+   
     public void ReleaseScore()
     {
         score = 0;
+    }
+    public void UpdateScore()
+    {
+        score++;
+    }
+    public void UpdateScore(int amount)
+    {
+        score += amount;
     }
     public void CheckNewBestScore()
     {
