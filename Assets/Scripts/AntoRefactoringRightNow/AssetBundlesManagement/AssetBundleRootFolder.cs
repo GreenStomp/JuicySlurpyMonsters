902b541e7a269ceafa29d7f4e90fc916dcbbf289
AntonioVerdiglione <anto.verdi27@gmail.com>
2018-05-16 14:19:38 +0200

    ScoreManagement,AssetsBundle Management, Polish

---
+++ Assets/Scripts/AntoRefactoringRightNow/AssetBundlesManagement/AssetBundleRootFolder.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using UnityEngine;
+
+public enum AssetBundleRootPath
+{
+    DataPath,
+    StreamingAssetsPath,
+    PersistentPath,
+}
+[CreateAssetMenu(fileName = "AssetBundleRootFolder")]
+public class AssetBundleRootFolder : ScriptableObject
+{
+    //were AssetBundles folder is stored
+    [SerializeField]
+    private AssetBundleRootPath rootPath = AssetBundleRootPath.DataPath;
+
+    //our assetBundles folder
+    [SerializeField]
+    private string assetBundleFolder = "AssetBundles";
+    //our platform Folder Name(ex:Windows) we build all bundles eachtime for different platforms
+    [SerializeField]
+    private string platformFolderName;
+    public string GetAssetBundlePath(string assetBundleName)
+    {
+        string directory = Path.Combine(GetRootPath(), assetBundleFolder);
+        return Path.Combine(directory, Path.Combine(platformFolderName, assetBundleName));
+    }
+    public string GetManifestBundlePath()
+    {
+        //this because when we build our manifest assetbundle will be called as the platform folder
+        return this.GetAssetBundlePath(platformFolderName);
+    }
+    private string GetRootPath()
+    {
+        string initialPath = string.Empty;
+        if (rootPath == AssetBundleRootPath.DataPath)
+            initialPath = Application.dataPath;
+        else if (rootPath == AssetBundleRootPath.StreamingAssetsPath)
+            initialPath = Application.streamingAssetsPath;
+        else if (rootPath == AssetBundleRootPath.PersistentPath)
+            initialPath = Application.persistentDataPath;
+        return initialPath;
+    }
+}
--- Assets/Scripts/Level/SpecialPlatform.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-public abstract class SpecialPlatform : MonoBehaviour
-{
-    public abstract void OnEnter(PlatformManager.Step step);
-    public abstract void OnExit(PlatformManager.Step step);
-    public abstract void OnStepTaken(PlatformManager.Step step, float prevPercentage);
-}
