1783a3b1383aac42c97ddfe03a526f7f3db9e0b9
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2018-05-22 01:22:20 +0200

    Fixed stuff after merge,removed some useless files

---
+++ Assets/Scripts/Player/Health.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+public class Health : MonoBehaviour
+{
+    public int NumberOfLives { get { return numberOfLives; } }
+    public int MaxNumberOfLives { get { return maxNumberOfLives; } }
+    [SerializeField]
+    private int maxNumberOfLives = 10;
+    [SerializeField]
+    private int numberOfLives = 3;
+    private void Update()
+    {
+        if (numberOfLives == 0)
+        {
+            GameOver();
+        }
+    }
+    public void DecreaseLife(int lives)
+    {
+        numberOfLives = Mathf.Max(numberOfLives - lives, 0);
+    }
+    public void IncreaseLife(int lives)
+    {
+        numberOfLives = Mathf.Min(numberOfLives + lives, maxNumberOfLives);
+    }
+    public void GameOver()
+    {
+        //Caricare scena nuova o resettare il livello
+        this.enabled = false;
+    }
+}
