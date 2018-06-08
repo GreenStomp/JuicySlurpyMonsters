bc5608b740de08a49b8efe39bf4fab016b46cc43
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2018-06-08 18:44:04 +0200

    Updated SOPRO, fixed scene bug and improved PlatManager

--- Assets/Scripts/Level/RenderingData.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-[CreateAssetMenu(fileName = "RenderingData" , menuName = "Rendering/Data")]
-public class RenderingData : ScriptableObject
-{
-    public Mesh Mesh;
-    public Material Material;
-}
--- Assets/Scripts/Tools/Tuple.cs
+++ Assets/Scripts/Tools/Tuple.cs
@@ -2,31 +2,12 @@
 {
     public T Item1;
     public K Item2;
-    public Tuple(T item1, K item2)
-    {
-        this.Item1 = item1;
-        this.Item2 = item2;
-    }
-    public Tuple()
-    {
-
-    }
 }
 public class Tuple<T, K, Z>
 {
     public T Item1;
     public K Item2;
     public Z Item3;
-    public Tuple(T item1, K item2, Z item3)
-    {
-        this.Item1 = item1;
-        this.Item2 = item2;
-        this.Item3 = item3;
-    }
-    public Tuple()
-    {
-
-    }
 }
 public class Tuple<T, K, Z, U>
 {
@@ -34,17 +15,6 @@ public class Tuple<T, K, Z, U>
     public K Item2;
     public Z Item3;
     public U Item4;
-    public Tuple(T item1, K item2, Z item3, U item4)
-    {
-        this.Item1 = item1;
-        this.Item2 = item2;
-        this.Item3 = item3;
-        this.Item4 = item4;
-    }
-    public Tuple()
-    {
-
-    }
 }
 public class Tuple<T, K, Z, U, Y>
 {
@@ -53,16 +23,4 @@ public class Tuple<T, K, Z, U, Y>
     public Z Item3;
     public U Item4;
     public Y Item5;
-    public Tuple(T item1, K item2, Z item3, U item4, Y item5)
-    {
-        this.Item1 = item1;
-        this.Item2 = item2;
-        this.Item3 = item3;
-        this.Item4 = item4;
-        this.Item5 = item5;
-    }
-    public Tuple()
-    {
-
-    }
 }
